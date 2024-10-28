d008cd3ca4f4f53bd7b7a0274ccaa8abe31cc18c
Narayan Babu <codevalley@live.com>
2024-10-28 21:59:26 +0530

    Fix message list

--- lib/features/chat/widgets/message_list.dart
+++ lib/features/chat/widgets/message_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:chat_app/features/chat/presentation/providers/chat_provider.dart';
-
+import '../presentation/providers/chat_provider.dart';
 
 class MessageList extends StatelessWidget {
   final List<ChatMessage> messages;
@@ -25,7 +24,8 @@ class MessageList extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 8.0),
             child: Center(
               child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
                 decoration: BoxDecoration(
                   color: Colors.grey[200],
                   borderRadius: BorderRadius.circular(12),
@@ -47,11 +47,14 @@ class MessageList extends StatelessWidget {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
           child: Align(
-            alignment: message.isUserMessage ? Alignment.centerRight : Alignment.centerLeft,
+            alignment: message.isUserMessage
+                ? Alignment.centerRight
+                : Alignment.centerLeft,
             child: Container(
               padding: const EdgeInsets.all(12),
               decoration: BoxDecoration(
-                color: message.isUserMessage ? Colors.blue[100] : Colors.grey[200],
+                color:
+                    message.isUserMessage ? Colors.blue[100] : Colors.grey[200],
                 borderRadius: BorderRadius.circular(12),
               ),
               child: Text(
